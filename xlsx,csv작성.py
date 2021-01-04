ac0c97ac0a91271b1c20bf26d80cfe4933fd6fb6
kjfdf <kjfdf1@gmail.com>
2021-01-04 14:39:41 +0900

    Add files via upload

---
+++ xlsx,csv작성.py
@@ -0,0 +1,28 @@
+import csv
+import openpyxl as xl
+import glob
+
+# xlsx파일 읽기
+files=glob.glob('C:/Users/SNUH/Desktop/유일한/*.xlsx')
+list=[]
+for file in files:
+    file=xl.load_workbook(file)
+    file_a=file.active
+    for r in file_a.rows:
+        if r[0].value is None:
+            continue
+        list.append([])
+        for c in r:
+            list[-1].append(c.value)
+        print(list[-1])
+
+# csv파일읽기
+files2=glob.glob('C:/Users/SNUH/Desktop/유일한/홍윤호교수님 연구/*.csv')
+list=[]
+for file in files2:
+    file=open(file, 'r', encoding='utf-8')
+    file_a=csv.reader(file)
+    for line in file_a:
+        print(line)
+    file.close()
+
